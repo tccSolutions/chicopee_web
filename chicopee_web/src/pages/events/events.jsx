573fa2d392d84dc0db5f9109025872cfb97413f3
Tim <tim@tccs.tech>
2022-04-13 08:18:43 -0400

    fixed api connection

--- chicopee_web/src/pages/events/events.jsx
+++ chicopee_web/src/pages/events/events.jsx
@@ -3,10 +3,8 @@ import React, {useState, useEffect} from "react";
 const Events = () => {
     const [events, setEvents] = useState([])
     const fetchEvents = () => {
-        fetch("https://chicopee-backend.herokuapp.com/api/all_events",{
-          method:'GET',
-          headers: {'Access-Control-Allow-Origin': '*'}
-        })
+        fetch("https://chicopee-backend.herokuapp.com/api/all_events"
+          )
         .then((response) => {
             return response.json();
         })
