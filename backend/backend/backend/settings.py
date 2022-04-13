11e96e5e386cd20ade21b04c0ee7726ca1ebc6d0
Tim <tim@tccs.tech>
2022-04-13 08:12:32 -0400

    update to settings.py

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -29,14 +29,8 @@ ALLOWED_HOSTS=[
         '*'
     ]
 
-CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = [
-    "https://tccsolutions.github.io/chicopee_web",
-    "https://chicopee-backend.herokuapp.com",
-    "http://localhost:8080",
-    "http://localhost:3000",
-    "http://127.0.0.1:9000"
-]
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 
 
