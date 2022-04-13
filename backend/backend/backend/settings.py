9bb36f43344db4966a92a1b41d082e48a8c8a417
Tim <tim@tccs.tech>
2022-04-13 06:46:18 -0400

    added cors support

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS=[
         '*'
     ]
 
-
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
+    'corsheaders'
     
 ]
 
@@ -58,6 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
   
 ]
 
