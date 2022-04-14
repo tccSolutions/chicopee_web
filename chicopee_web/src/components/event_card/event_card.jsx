490e1c5f6210b0d231f88e598173d8b8fe0b0c00
Tim <tim@tccs.tech>
2022-04-14 08:37:54 -0400

    update to events page

--- chicopee_web/src/components/event_card/event_card.jsx
+++ chicopee_web/src/components/event_card/event_card.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled, { keyframes } from "styled-components";
 import { rollIn, fadeInDownBig } from "react-animations";
+import { fadeInLeft } from "react-animations";
+import { fadeInRight } from "react-animations";
 
-const EventCard = ({ timer, title, start_date, end_date, url, image_url }) => {
-  const Animation_1 = keyframes`${rollIn}`;
-  const Animation_2 = keyframes`${fadeInDownBig}`;
-  const chosenAnimation = Animation_1;
+const EventCard = ({ count,timer, title, start_date, end_date, url, image_url }) => {
+  
+  const Animation_1 = keyframes`${fadeInLeft}`;
+  const Animation_2 = keyframes`${fadeInDownBig}`; 
+  const [animation, setAnimation]= useState()
+  
+  useEffect(()=>{
+ 
+    setAnimation(()=>{
+      if(count % 2 === 0 || count % 2 === 1){
+        console.log('Animation_1')
+        return Animation_1       
+      }
+      console.log('Animation_2')
+      return Animation_2
+    })
+    
+  },[])
+    
+ 
   let AnimatedDiv = styled.div`
-    animation: ${timer *.5}s ${chosenAnimation};
-  `;
+      animation: ${timer * 0.5}s ${animation};
+    `;
   return (
-    <AnimatedDiv class="mb-5 shadow text-light" style={{'width':'', 'height':'15em'}}>
-     <a class='btn bg-dark text-light' href={url} target="_blank">
-     <div>
-      <img src={image_url} alt='...'/>
-     </div>
-       <div >
-         <h5>{title}</h5>
-         <h6>{start_date}-{end_date}</h6>
-       </div>
-     </a>
-    </AnimatedDiv>
+    <div
+      class="mb-2 border border-light shadow bg-dark text-light"
+      style={{height: "15em" }}
+    >
+      <AnimatedDiv class="">
+        <div className="mt-3">
+          <img src={image_url} alt="..." style={{'maxWidth':'25rem'}} />
+        </div>
+        <div>
+          <h5>{title}</h5>
+          <h6>
+            {start_date}-{end_date}
+          </h6>
+        </div>
+      </AnimatedDiv>
+    </div>
   );
 };
 
--- chicopee_web/src/pages/events/events.jsx
+++ chicopee_web/src/pages/events/events.jsx
@@ -1,45 +1,50 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import EventCard from "../../components/event_card/event_card";
 
 const Events = () => {
-    const [events, setEvents] = useState([])
-    const fetchEvents = () => {
-        fetch("https:chicopee-backend.herokuapp.com/api/all_events"
-          )
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
+  const [events, setEvents] = useState([]);
+  const fetchEvents = () => {
+    fetch("http://127.0.0.1:8000/api/all_events")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
         setEvents(data);
-        console.log(events)
+        console.log(events);
       });
-    };
+  };
 
-    useEffect(()=>{
-      fetchEvents()
-  },[])
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
-
+  let count = 0;
   return (
-     <div className='mt-5 '>
-       <div>
-         <h1>Scheduled Events</h1>
+    <div className="mt-5 container-fluid ">
+      <div>
+        <h1>Scheduled Events</h1>
       </div>
-      <div className=''>
-      {events.map(event=>(
-         <EventCard 
-         key={event.id} 
-         timer={event.id} 
-         title={event.title}
-         url={event.url}
-         image_url = {event.image_url}
-         start_date={event.start_date}
-         end_date={event.end_date}/>
-       ))}
+      <div className="row  justify-content-center">
+        {events.map((event) => {
+          count += 1;
+          return (
+            <div className="col-md-8 rounded justify-content-center">
+              <EventCard
+                key={event.id}
+                count={count}
+                timer={(count += 1)}
+                title={event.title}
+                url={event.url}
+                image_url={event.image_url}
+                start_date={event.start_date}
+                end_date={event.end_date}
+              />
+            </div>
+          );
+        })}
       </div>
-       
-     </div>
+    </div>
   );
-}
+};
 
 export default Events;
