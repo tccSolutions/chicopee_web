9e6e68b5f435299fc3c52801e276ee802f159378
Tim <tim@tccs.tech>
2022-04-14 15:10:18 -0400

    settings.py

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -102,7 +102,7 @@ DATABASES = {
         'USER': os.getenv('USER'),
         'PASSWORD': os.getenv('PASSWORD'),
         'HOST': os.getenv('HOST'),
-        'PORT': os.getenv('PORT'),
+        'PORT': 5432,
     }
 }
 
