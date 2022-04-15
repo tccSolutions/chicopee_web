dd99764db153cf7de95a5eea433dc4d13fe1a612
Tim <tim@tccs.tech>
2022-04-15 17:34:15 -0400

    update to events page and added contact page

--- chicopee_web/src/index.js
+++ chicopee_web/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createRoot } from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -9,7 +9,7 @@ import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<HashRouter><App tab="home" /></HashRouter>);
+root.render(<BrowserRouter><App tab="home" /></BrowserRouter>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
