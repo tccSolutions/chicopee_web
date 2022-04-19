66d1b13d291e469b0cbd977803968d08132db979
tccSolutions <tim@tccs.tech>
2022-04-18 22:23:58 -0400

    added loading screen for events

--- chicopee_web/src/components/event_sidebar/event_sidebar.jsx
+++ chicopee_web/src/components/event_sidebar/event_sidebar.jsx
@@ -5,13 +5,12 @@ import {rollIn, fadeInDownBig} from "react-animations";
 
 const EventSidebar = () => {
   const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true)
   let count = 4;
   const Animation_1 = keyframes`${rollIn}`;
   const Animation_2 = keyframes`${fadeInDownBig}`;
   const chosenAnimation = Animation_2;
-  let AnimatedDiv = styled.div`
-    animation: ${count}s ${chosenAnimation};
-  `;
+ 
 
   const fetchEvents = () => {
     fetch("https://chicopee-backend.herokuapp.com/api/monthly_events")
@@ -20,37 +19,53 @@ const EventSidebar = () => {
       })
       .then((data) => {
         setEvents(data);
+        setLoading(false)
       });
   };
 
   useEffect(() => {
-    fetchEvents();
+    fetchEvents()
+    
   }, []);
 
   return (
-    <section className="">
-      <h4 style={{ 'borderBottom': '1px dotted black' }}>Featured This Month</h4>
-
-      {events.map((event, index) => {
-        count -= 0.5;
-        AnimatedDiv = styled.div`
+    <div className='mt-5'>
+      <div>
+        <h4>Happening This Month</h4>
+        <hr/>
+      </div>
+    <>
+    {loading === true ? (
+      <div className='mt-5'>
+      <h3>Please Wait</h3>
+      <img src="https://pictures.dealer.com/c/covingtonfordfd/0870/6990b341e1a900977817d12e338ee455x.jpg?impolicy=resize&w=250"/>
+      <h4>Gathering Events...</h4>
+      </div>
+    ):(
+      <div>
+        {events.map(event=>{
+          count += -.5;
+          let AnimatedDiv = styled.div`
           animation: ${count}s ${chosenAnimation};
-        `;
+        `;          
+          return(
+            <AnimatedDiv>
+              <SidebarEvent
+          title={event.title}
+          img={event.image_url}
+          start_date={event.start_date}
+          end_date={event.end_date}
+          url={event.url}
+          />
 
-        return (
-          <AnimatedDiv key={index}>
-            <SidebarEvent           
-              key={event.id}
-              title={event.title}
-              start_date={event.start_date}
-              end_date={event.end_date}
-              url={event.url}
-              img={event.image_url}
-            />
-          </AnimatedDiv>
-        );
-      })}
-    </section>
+            </AnimatedDiv>
+          
+          )
+        })}
+      </div>
+    )}
+    </>
+    </div>
   );
 };
 
--- chicopee_web/src/pages/events/events.jsx
+++ chicopee_web/src/pages/events/events.jsx
@@ -3,23 +3,29 @@ import EventCard from "../../components/event_card/event_card";
 
 const Events = () => {
   const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true)
   const fetchEvents = () => {
     fetch("https://chicopee-backend.herokuapp.com/api/all_events")
       .then((response) => {
         return response.json();
       })
-      .then((data) => {
+      .then((data) => {       
         setEvents(data);
+        setLoading(false)
+        
         
       });
   };
 
   useEffect(() => {
-    fetchEvents();
+   fetchEvents()
+   
   }, []);
 
   let count = 0;
   return (
+    <>
+    {loading === false ? (
     <div className="mt-5 container-fluid ">
       <div>
         <h1>Scheduled Events</h1>
@@ -43,6 +49,14 @@ const Events = () => {
         })}
       </div>
     </div>
+    ):(
+      <div className='position-absolute top-50 start-50 translate-middle'>
+        <h3>Please Wait</h3>
+        <img src="https://img1.picmix.com/output/stamp/thumb/4/3/5/0/270534_fc964.gif"/>
+        <h4>Gathering Events...</h4>
+      </div>
+    )}
+    </>
   );
 };
 
